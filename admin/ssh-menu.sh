e9bd40957a0aab4b0833d4388d45f40780b76bc1
EkromSSH <ikromlotae@gmail.com>
2026-07-20 23:03:36 +0000

    Add SSH submenu script

---
+++ admin/ssh-menu.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# SSH Management Menu - EkromSSH VPN
+clear
+grenbo="\e[92;1m"
+NC='\033[0m'
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│$NC\033[42m            เมนูจัดการ SSH              $NC"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e "\033[1;93m┌──────────────────────────────────────────┐\033[0m"
+echo -e "\033[1;93m│  ${grenbo}1.${NC} \033[0;36mสร้างบัญชี SSH OVPN${NC}"
+echo -e "\033[1;93m│  ${grenbo}2.${NC} \033[0;36mลบบัญชี SSH OVPN${NC}"
+echo -e "\033[1;93m│  ${grenbo}3.${NC} \033[0;36mต่ออายุบัญชี SSH OVPN${NC}"
+echo -e "\033[1;93m│  ${grenbo}4.${NC} \033[0;36mตรวจสอบ SSH OVPN${NC}"
+echo -e "\033[1;93m│  ${grenbo}5.${NC} \033[0;36mเปลี่ยนพอร์ต SSH WS${NC}"
+echo -e "\033[1;93m└──────────────────────────────────────────┘\033[0m"
+echo -e ""
+read -p "เลือกจากตัวเลือก [ 1 - 5 ] : " menu
+case $menu in
+1) add-ssh ;;
+2) del-ssh ;;
+3) renew-ssh ;;
+4) cek-ssh ;;
+5) change-port ;;
+*) ssh ;;
+esac
