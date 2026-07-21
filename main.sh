e3888c7f681c2701c49096054ae6914dcf515650
EkromSSH <ikromlotae@gmail.com>
2026-07-21 07:04:41 +0000

    Fix xray download URL (use official XTLS release)

--- main.sh
+++ main.sh
@@ -156,7 +156,8 @@ function install_xray(){
     curl -s ipinfo.io/city >> /etc/xray/city
     curl -s ipinfo.io/org | cut -d " " -f 2-10 >> /etc/xray/isp
     xray_latest="$(curl -s https://api.github.com/repos/dharak36/Xray-core/releases | grep tag_name | sed -E 's/.*"v(.*)".*/\1/' | head -n 1)"
-    xraycore_link="https://github.com/EkromSSH/Xcore-custompath/releases/download/Xray-linux-64-v1.6.5.1/Xray-linux-64-v1.6.5.1"
+    xray_latest="$(curl -s https://api.github.com/repos/XTLS/Xray-core/releases/latest | grep tag_name | cut -d\" -f4)"
+    xraycore_link="https://github.com/XTLS/Xray-core/releases/download/$xray_latest/Xray-linux-64.zip"
     curl -sL "$xraycore_link" -o xray
 #    unzip -q xray.zip && rm -rf xray.zip
     mv xray /usr/sbin/xray
